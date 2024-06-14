dc09dd4c28b2006648d8fca640ecfa9b04fc5ea0
SouRavDas713 <sdsourav713@gmail.com>
2024-06-14 10:58:25 +0600

    Graph By AdjacencyList

--- AdjacencyList.cpp
+++ AdjacencyList.cpp
@@ -1,11 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std ;
-//V*V matrix
+
 
 const int N = 1e3+10 ;
-int graph[N][N] ;
-// O(n^2)
-// if n=10^6 Then it impossible to store the matrix
+int graph1[N][N] ;
+// O(V+E)
+// 
+
+vector<int> graph2[N] ; 
 
 int main()
 {
@@ -16,17 +18,32 @@ int main()
     {
         int v1 , v2 ; 
         cin>>v1>>v2 ; 
-        graph[v1][v2] = 1 ; 
-        graph[v2][v1] = 1 ; 
+        graph1[v1][v2] = 1 ; 
+        graph1[v2][v1] = 1 ; 
+
+        graph2[v1].push_back(v2) ; 
+        graph2[v2].push_back(v1) ; 
     }
 
     // Output the adjacency matrix
-    
+     cout << "Adjacency Matrix:" << endl;
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= n; j++) 
         {
-            cout << graph[i][j] << " ";
+            cout << graph1[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+     // Output the adjacency list
+    cout << "Adjacency List:" << endl;
+    for (int i = 1; i <= n; i++)
+    {
+        cout << i << ": ";
+        for (int v : graph2[i])
+        {
+            cout << v << " ";
         }
         cout << endl;
     }
