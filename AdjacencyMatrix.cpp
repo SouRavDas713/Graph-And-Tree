bb4b06c06e6cf3e08b7144416d7304c6d2491b3a
SouRavDas713 <sdsourav713@gmail.com>
2024-06-14 10:33:20 +0600

    basic input output by AdjacencyMatrix

---
+++ AdjacencyMatrix.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std ;
+//V*V matrix
+
+const int N = 1e3+10 ;
+int graph[N][N] ; 
+// O(n^2)
+// if n=10^6 Then it impossible to store the matrix
+
+int main()
+{
+    int n , m ; // vertex and Edge 
+    cin>>n>>m ; 
+  
+    for(int i=0;i<m;i++)
+    {
+        int v1 , v2 ; 
+        cin>>v1>>v2 ; 
+        graph[v1][v2] = 1 ; 
+        graph[v2][v1] = 1 ; 
+    }
+
+    // Output the adjacency matrix
+    
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++) 
+        {
+            cout << graph[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0 ; 
+}
